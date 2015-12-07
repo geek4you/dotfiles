1699feb9dae4d0643a0e1fcfb7263d710dc31111
Geek 4 You <chanumolu@yahoo.com>
2015-12-06 22:21:21 -0800

    setup-mac script

---
+++ setup-mac.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap homebrew/x11
+
+brew update
+
+BREW_APPS="$(cat brew.txt)"
+
+for BREW_APP in ${BREW_APPS}; do
+  echo "Installing \"$BREW_APP\"..."
+  brew install --force $BREW_APP
+  echo "...installed \"$BREW_APP\"."
+done
+
+brew tap caskroom/versions
+
+brew cask update
+
+CASK_APPS="$(cat brew-cask.txt)"
+
+for CASK_APP in ${CASK_APPS}; do
+  echo "Installing \"$CASK_APP\"..."
+  brew cask install --force $CASK_APP
+  echo "...installed \"$CASK_APP\"."
+done
